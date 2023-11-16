70497ae14ffb5dc0ae2e54c104d3640bc8bb67b1
Giovanni Gardin <2914344+ggardin@users.noreply.github.com>
2023-11-16 21:32:52 +0000

    Add version handler

---
+++ .github/workflows/scripts/file_version_handler.py
@@ -0,0 +1,52 @@
+import sys
+import glob
+import re
+import subprocess
+
+repo_url = "https://github.com/Carraro-Riccardo/test-python-versioning"
+target_directory = "./tmp"
+branch_name = "main"
+subprocess.run(["git", "clone", "--branch",  branch_name, repo_url, target_directory,  "--quiet"], check=True)
+
+file_path  = sys.argv[1]
+file_name   = sys.argv[2]
+str_version = "1.0.0"
+
+search_pattern = f"{file_name}*.pdf"
+matching_file = glob.glob("./tmp/"+file_path+"/"+search_pattern)
+re_pattern = r"_v(\d+\.\d+\.\d+)\.pdf"
+
+match = False
+if len(matching_file) != 0:
+    match = re.search(re_pattern,"./tmp/"+file_path+"/"+matching_file[0])
+
+if match:
+    str_version = match.group(1)
+
+    version_components = str_version.split('.')
+    x = int(version_components[0])
+    y = int(version_components[1])
+    z = int(version_components[2])
+
+    new_file_content  = ""
+    old_file_content = ""
+
+    with open(file_path+"/"+file_name+".typ") as file:
+        new_file_content = file.read()
+
+    with open("./tmp/"+file_path+"/"+file_name+".typ") as file:
+        old_file_content = file.read()
+
+    def diff_letters(first, second,x,y,z):
+        if first != second:
+            z = z + 1
+        if first.count("=") < second.count("="):
+            y = y + 1
+            z = 0
+        return str(x) + "." + str(y) + "." + str(z)
+
+    new_version = diff_letters(old_file_content, new_file_content,x,y,z)
+
+    print(new_version)
+else:
+    print(str_version)

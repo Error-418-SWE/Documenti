4c1323182120b0ab3bd27810eebf8c735893a516
Giovanni <2914344+ggardin@users.noreply.github.com>
2024-02-18 09:08:36 +0100

    Definito URL capitolato tra le risorse comuni

--- common.ts
+++ common.ts
@@ -4,6 +4,7 @@ export namespace Common {
 	export const githubPagesURL = 'https://ggardin.github.io';
 	export const documentsRepositoryName = 'Documenti';
 	export const documentsRepositoryURL = 'https://github.com/Error-418-SWE/Documenti/tree/main';
+	export const specificationsDocumentURL = 'https://www.math.unipd.it/~tullio/IS-1/2023/Progetto/C5.pdf';
 	export const dashboardURL = 'https://error418swe.grafana.net/public-dashboards/9392efccc5a5427c850fc9ec81df7dff';
 	export const softwareRepositorURL = '';
 	export const projectName = 'WMS3';
